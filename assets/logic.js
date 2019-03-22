a9cbcffd88f01bfdb574be7e6a7fc2abaa420c01
steven <steven@stevens-MacBook-Pro.local>
2019-03-22 07:48:17 -0600

    getting started

---
+++ assets/logic.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCeuHNkIttZaGFaPhRiA6aNqeeZaIwyrfk",
+    authDomain: "trains-26e34.firebaseapp.com",
+    databaseURL: "https://trains-26e34.firebaseio.com",
+    projectId: "trains-26e34",
+    storageBucket: "trains-26e34.appspot.com",
+    messagingSenderId: "514624912530"
+  };
+  firebase.initializeApp(config);
