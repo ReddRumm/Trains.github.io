0103045366807b5aaff0cc926c3f7a9d0e2a131f
ReddRumm <stevenmwebdev@gmail.com>
2019-03-25 07:43:00 -0600

    slowing making it through

--- assets/logic.js
+++ assets/logic.js
@@ -7,3 +7,40 @@ var config = {
     messagingSenderId: "514624912530"
 };
 firebase.initializeApp(config);
+
+
+
+/**
+ * Saves a new post to the Firebase DB.
+ */
+
+function trainSchedule(trainId, destination, frequency, firstTrTime) {
+    // A post entry.
+    var postData = {
+        trainId: trainId,
+        destination: destination,
+        frequency: frequency,
+        firstTrTime: firstTrTime,
+    };
+    console.log(postData);
+}
+
+// Saves message on form submit.
+$('.sBtn').onsubmit = function (e) {
+    e.preventDefault();
+    var trainName = $('trainName').val().trim();
+    var destination = $('destination').val().trim();
+    var firstTrainTime = $('firstTrTime').val().trim();
+    var frequency = $('frequency').val().trim();
+
+    var tableDataTrainID = $('<td id="trainID">');
+    var tableDataDestination = $('<td id="dest">');
+    var tableDataFrequency = $('<td id="frequ">');
+    var tableDatafirstTrainTime = $('<td id="firstTrainTime">');
+
+
+    tableDataTrainID.append(trainName);
+    tableDataDestination.append(destination);
+    tableDataFrequency.append(frequency);
+    tableDatafirstTrainTime.append(firstTrainTime);
+};
