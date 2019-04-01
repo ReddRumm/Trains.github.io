0ef1ca10b9b9c1948da21dd3e0e5a99ea622f758
ReddRumm <stevenmwebdev@gmail.com>
2019-04-01 11:48:28 -0600

    Git 'er dun

--- assets/logic.js
+++ assets/logic.js
@@ -1,19 +1,31 @@
+// *Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCeuHNkIttZaGFaPhRiA6aNqeeZaIwyrfk",
+    authDomain: "trains-26e34.firebaseapp.com",
+    databaseURL: "https://trains-26e34.firebaseio.com",
+    projectId: "trains-26e34",
+    storageBucket: "trains-26e34.appspot.com",
+    messagingSenderId: "514624912530"
+};
+
+firebase.initializeApp(config);
+
 var database = firebase.database();
 
 function trainSchedule(trainId, destination, firstTrTime, frequency) {
-    // A post entry.
+    // *A post entry.
     var postData = {
         trainId: trainId,
         destination: destination,
         firstTrTime: firstTrTime,
         frequency: frequency,
-        // nextTrain: nxtTrain,
+        // *nextTrain: nxtTrain,
     };
 
-    // Get a key for a new Post.
+    // *Get a key for a new Post.
     var newPostKey = firebase.database().ref().child('posts').push().key;
 
-    // Write the new post's data simultaneously in the posts list and the user's post list.
+    // *Write the new post's data simultaneously in the posts list and the user's post list.
     var updates = {};
     updates['/posts/' + newPostKey] = postData;
     updates['/user-posts/' + trainId + '/' + newPostKey] = postData;
@@ -22,13 +34,13 @@ function trainSchedule(trainId, destination, firstTrTime, frequency) {
     return firebase.database().ref().update(updates);
 }
 
-// I can not get this is produce the currect time.
-// I will come back to this. 
-// function addMinutes(date, minutes) {
-//     return new Date(date.getTime() + minutes * 60000);
-// }
+// !I can not get this is produce the currect time.
+// !I will come back to this. 
+// !function addMinutes(date, minutes) {
+//     !return new Date(date.getTime() + minutes * 60000);
+// !};
 
-// Saves message on form submit.
+// *Saves infomation on form submit.
 $('.btn').on('click', function (e) {
     e.preventDefault();
 
@@ -39,26 +51,26 @@ $('.btn').on('click', function (e) {
 
     trainSchedule(trainName, destination, firstTrainTime, frequency);
 
-    // var nxtTrain = addMinutes(firstTrainTime, frequency);
+    // ?var nxtTrain = addMinutes(firstTrainTime, frequency);
 
     var allTableData = $('<tr id="tableRow">');
     var tableDataTrainID = $('<td id="trainID">');
     var tableDataDestination = $('<td id="dest">');
     var tableDatafirstTrainTime = $('<td id="firstTrainTime">');
     var tableDataFrequency = $('<td id="frequ">');
-    // var tableDataNxtTrain = $('<td id="nxtTrain">');
+    // ?var tableDataNxtTrain = $('<td id="nxtTrain">');
 
     tableDataTrainID.append(trainName);
     tableDataDestination.append(destination);
     tableDatafirstTrainTime.append(firstTrainTime);
     tableDataFrequency.append(frequency);
-    // tableDataNxtTrain.append(nxtTrain);
+    // ?tableDataNxtTrain.append(nxtTrain);
 
     allTableData.append(tableDataTrainID);
     allTableData.append(tableDataDestination);
     allTableData.append(tableDatafirstTrainTime);
     allTableData.append(tableDataFrequency);
-    // allTableData.append(tableDataNxtTrain);
+    // ?allTableData.append(tableDataNxtTrain);
 
     $('.tableBody').append(allTableData);
 
@@ -73,5 +85,5 @@ $('.btn').on('click', function (e) {
     console.log(destination);
     console.log(firstTrainTime);
     console.log(frequency);
-    // console.log(nxtTrain);
+    // ?console.log(nxtTrain);
 });
